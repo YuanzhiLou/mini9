d2281e7902bc05e9c69e4eb7ba6401d9860be669
YuanzhiLou <157557945+YuanzhiLou@users.noreply.github.com>
2024-04-05 03:12:47 -0400

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 
-model = pipeline("text-generation", model="distilgpt2")
+model = pipeline("text-generation", model="openai-gpt")
 input_text = st.text_area("Enter Text", height=100)
 generate_button = st.button("Generate Text")
 if generate_button:
